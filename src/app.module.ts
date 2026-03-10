d741bd29ca7eb37dfb90effa494f7b0dcc497fee
palash2397 <palashpatil55@gmail.com>
2026-03-10 12:22:35 +0530

    Add user module and update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
 
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [],
