e4f5a5212f0b14a2518c33cd7b5d67aced437b93
palash2397 <palashpatil55@gmail.com>
2026-03-10 10:43:06 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import { ValidationPipe } from '@nestjs/common';
+
+import morgan from 'morgan';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(morgan('dev'));
+
+  // enable global validation for DTOs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // cors
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
+
+
+  await app.listen(process.env.PORT ?? 4005, '127.0.0.1');
+  console.log(`🚀 Server is running on port ${process.env.PORT}`);
+}
+bootstrap();
